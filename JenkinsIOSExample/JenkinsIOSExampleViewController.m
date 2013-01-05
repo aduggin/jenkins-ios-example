39c97f038f3e0ea54cade771949c28830eac9b20
Alistair Duggin <alistair.duggin@btinternet.com>
2013-01-05 02:06:28 +0000

    Fixing error

--- JenkinsIOSExample/JenkinsIOSExampleViewController.m
+++ JenkinsIOSExample/JenkinsIOSExampleViewController.m
@@ -19,7 +19,9 @@
 {
     // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
-
+    
+    // Release any cached data, images, etc that aren't in use.
+}
 
 #pragma mark - View lifecycle
 
